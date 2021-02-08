a277461015a84a899a6b846d8322046550f9e32e
jun-k0 <wnsrlsladlek@naver.com>
2021-02-08 16:01:29 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class ApiController {
+    // 방문한 사용자의 검색기록 ㄱㄱ
+}
