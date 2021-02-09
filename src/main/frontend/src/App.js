2d48d667ea9019df00555ce9b99a048b9effc324
jun-k0 <wnsrlsladlek@naver.com>
2021-02-09 22:39:12 +0900

    front 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from "axios";
+import History from "./History";
 
 function App() {
+  const [name,setName]=useState();
+  const search=()=>{
+    console.log({name});
+    axios.post('/search', {
+      SummonerName: {name}
+      })
+    .then(function (response) {
+      console.log(response);
+     })
+    .catch(function (error) {
+      console.log(error);
+      });
+    // 새로운 페이지 렌더링?
+    <History
+      name={name}
+    />
+  }
+  const change=(e)=>{
+    console.log(e.target.value);
+    setName(e.target.value);
+  }
   return (
     <div className="App">
-        front 단 rr
+      <header>koGG</header>
+      <section>
+        <input type="text" placeholder="아이디를 입력하세요." value={name} onChange={change}></input>
+        <input type="button" value="검색!" onClick={search} ></input>
+      </section>
     </div>
   );
 }
---
+++ src/main/frontend/src/History.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function History({name}){
+    return(
+        <div>
+            {name}
+        </div>
+    )
+}
+
+export default History;
